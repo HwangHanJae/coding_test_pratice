a78089a5b735f4e293871f130931cc8b8c8f800a
HwangHanJae <60374463+HwangHanJae@users.noreply.github.com>
2021-12-31 16:27:12 +0900

    답안을 보고 했기 때문에 해결하지 못함
    유니코드를 통하여 문자열을 숫자로 바꾸는 알고리즘을 생각하지 못했고, steps를 구현할 생각하지 못함

--- implementation/왕실의나이트/answer.py
+++ implementation/왕실의나이트/answer.py
@@ -26,3 +26,32 @@ for step in steps:
 print(count)
 end_time = time.time()
 print("시간 : ",end_time-start_time)
+
+#답
+input_data = input()
+
+import time
+start_time = time.time()
+
+row = int(input_data[1])
+#들어오는 열을 유니코드로 바꾸어준뒤 유니코드 a를 빼주고 1을 더해줌
+#들어오는 열을 숫자로 바꿀수 있음
+col = int(ord(input_data[0])) - int(ord('a'))+1
+
+#나이트가 이동할 수 있는 8가지 방향
+steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+
+count = 0
+
+#8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+for step in steps:
+  #이동하고자 하는 위치를 확인
+  next_row = row + step[0]
+  next_col  = col + step[1]
+  #해당 위치로 이동이 가능하다면 카운트 증가
+  if next_row >= 1 and next_row <= 8 and next_col >=1 and next_col<=8:
+    count+=1
+
+print(count)
+end_time = time.time()
+print("시간 : ",end_time-start_time)
