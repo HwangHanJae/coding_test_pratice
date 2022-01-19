4fd6efc8dc765ea44ce3f65132bf39ef2d304377
HwangHanJae <60374463+HwangHanJae@users.noreply.github.com>
2022-01-19 19:19:32 +0900

    Delete 백준 directory

--- 백준/거스름돈/5585.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-coins = [500,100,50,10,5,1]
-n = 1000 - n
-count = 0
-for coin in coins:
-    count += n // coin
-    n %= coin
-print(count)
--- 백준/그대로 출력하기/11718.py
+++
@@ -1,7 +0,0 @@
-import sys
-while True:
-    data = sys.stdin.readline().rstrip()
-    if not data:
-        break
-    print(data)
-        
