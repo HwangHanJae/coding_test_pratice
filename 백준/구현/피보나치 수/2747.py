829d01e2094b921063d4f5d1bfc4bcfca6f3cff1
HwangHanJae <60374463+HwangHanJae@users.noreply.github.com>
2022-03-04 17:13:16 +0900

    Add files via upload

---
+++ 백준/구현/피보나치 수/2747.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [0 for _ in range(n+1)]
+for i in range(n+1):
+    if i == 0:
+        d[i] = 0
+    elif i == 1:
+        d[i] = 1
+    else:
+        d[i] = d[i-1] + d[i-2]
+print(d[n])
