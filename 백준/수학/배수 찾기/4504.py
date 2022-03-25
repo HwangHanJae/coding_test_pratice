cc855891dfebe9d8fe3dcd1430d017b89bbdda27
HwangHanJae <60374463+HwangHanJae@users.noreply.github.com>
2022-03-25 19:35:24 +0900

    Add files via upload

---
+++ 백준/수학/배수 찾기/4504.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+while True:
+  a = int(input())
+  if a == 0:
+    break
+  if a % n == 0:
+    print("{} is a multiple of {}.".format(a, n))
+  else:
+    print("{} is NOT a multiple of {}.".format(a, n))
